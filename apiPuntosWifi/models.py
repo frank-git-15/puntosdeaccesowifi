355a587c241fe1986bb525a637fe21d88363bec0
Francisco15Lydo <113368085+Francisco15Lydo@users.noreply.github.com>
2024-02-13 07:39:30 -0600

    Se creo el modelo para los puntos de acceso wifi y se registro, ya se puede ver desde la consola admin

--- apiPuntosWifi/models.py
+++ apiPuntosWifi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class PuntoDeAccesoWifi(models.Model):
+    id = models.CharField(max_length=100,primary_key=True)
+    programa = models.CharField(max_length=50)
+    fecha_instalacion = models.DateField()
+    latitud = models.FloatField()
+    longitud = models.FloatField()
+    colonia = models.CharField(max_length = 100)
+    alcaldia = models.CharField(max_length = 50)
+
+    def __str__(self) -> str:
+        return self.id
+
+
