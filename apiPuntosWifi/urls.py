0f571a449452a212abf4a5120c0331b6b39dba64
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-02-16 13:14:39 -0600

    comentarios innecesarios eliminados

--- apiPuntosWifi/urls.py
+++ apiPuntosWifi/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from .schema import schema
 urlpatterns = [
+    #Este es el endpoint que usaremos ipaddress:puerto/graphql
+    #Con esto se abrira una vista donde podremos hacer la consultas
+    #Sí es local host se verira asi la ruta http://127.0.0.1:8000/graphql
     path("graphql",GraphQLView.as_view(graphiql=True, schema=schema))
 ]
