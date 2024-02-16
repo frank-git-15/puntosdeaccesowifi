a4bebe05570ed30ad73d100619e1f9c50e71c9ed
frank-git-15 <franciscoalvarrera15@gmail.com>
2024-02-16 13:14:27 -0600

    comentarios innecesarios eliminados

--- DataPipeline/urls.py
+++ DataPipeline/urls.py
@@ -21,9 +21,6 @@ from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Con esta linea se importan las urls de la aplicacion apiPunrosWifi, en la cual estan el endpoint de graphql
     path("",include('apiPuntosWifi.urls')),
-    #Este es el endpoint que usaremos ipaddress:puerto/graphql
-    #Con esto se abrira una vista donde podremos hacer la consultas
-    #Sí es local host se verira asi la ruta http://127.0.0.1:8000/graphql
-    #path("graphql",GraphQLView.as_view(graphiql=True, schema=schema))
 ]
